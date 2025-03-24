c9889e1bbe31344d8ccfa9d5ea38c0267414afb2
dnwldla <leeemjw@naver.com>
2025-03-24 20:46:45 +0900

    first commit

---
+++ src/main/java/com/example/emotionbot/EmotionbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotionbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emotionbot/EmotionbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emotionbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
