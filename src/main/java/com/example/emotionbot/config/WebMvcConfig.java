32e1c85b2cc25f9c78e48db30368c0126598b4b0
dnwldla <leeemjw@naver.com>
2025-03-28 13:14:57 +0900

    feat: 커스텀 로그인 어노테이션 추가

--- src/main/java/com/example/emotionbot/config/WebMvcConfig.java
+++ src/main/java/com/example/emotionbot/config/WebMvcConfig.java
@@ -1,12 +1,16 @@
 package com.example.emotionbot.config;
 
+import com.example.emotionbot.common.resolver.MemberArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -28,5 +32,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    /**
+     ArgumentResolver 등록
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberArgumentResolver());
     }
 }
