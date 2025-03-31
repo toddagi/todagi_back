fcdf39e2db23fec06ddec83ce40500041304b3f1
dnwldla <leeemjw@naver.com>
2025-03-31 23:37:05 +0900

    feat: ci/cd 테스트

--- src/main/java/com/example/emotionbot/api/member/rest/TestController.java
+++ src/main/java/com/example/emotionbot/api/member/rest/TestController.java
@@ -1,14 +1,15 @@
 package com.example.emotionbot.api.member.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @GetMapping("/")
-    public String hello(){
-        return "Hello World";
+    public String hello() {
+        return "..?";
     }
 
 
