e838043186798c53822d7e759acfa68b97e3b506
dnwldla <leeemjw@naver.com>
2025-04-03 13:13:27 +0900

    feat: Chat 테이블에  sender 컬럼 추가 보낸이가 사용자인지, 봇인지 구별하기 위함입니다

--- src/main/java/com/example/emotionbot/api/chat/entity/Chat.java
+++ src/main/java/com/example/emotionbot/api/chat/entity/Chat.java
@@ -24,22 +24,27 @@ public class Chat {
     @ManyToOne(fetch = FetchType.LAZY)
     private ChatRoom chatRoom;
 
-    @JoinColumn(name = "member_id", columnDefinition = "bigint(20)", nullable = false)
+    @JoinColumn(name = "member_id", columnDefinition = "bigint(20)")
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
     @Column(name = "message", columnDefinition = "varchar(100)", nullable = false)
     private String message;
 
+    @Column(name = "sender", columnDefinition = "varchar(10)", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Sender sender;
+
     @Column(name = "send_time", columnDefinition = "timestamp", nullable = false)
     private LocalDateTime sendTime;
 
     @Builder
-    public Chat(Long id, ChatRoom chatRoom, Member member, String message, LocalDateTime sendTime) {
+    public Chat(Long id, ChatRoom chatRoom, Member member, String message, Sender sender,LocalDateTime sendTime) {
         this.id = id;
         this.chatRoom = chatRoom;
         this.member = member;
         this.message = message;
+        this.sender=sender;
         this.sendTime = sendTime;
     }
 }
