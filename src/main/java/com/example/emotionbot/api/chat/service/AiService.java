fb6f57cadb08b91085b0924c697079b9f06048c8
hyebin <shin378378@naver.com>
2025-04-11 13:46:28 +0900

    Refactor : 로그 찍기

--- src/main/java/com/example/emotionbot/api/chat/service/AiService.java
+++ src/main/java/com/example/emotionbot/api/chat/service/AiService.java
@@ -2,6 +2,7 @@ package com.example.emotionbot.api.chat.service;
 
 import com.example.emotionbot.api.chat.dto.request.ChatSendRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AiService {
@@ -20,14 +22,21 @@ public class AiService {
     private String aiServerUrl;
 
     public String askToAi(ChatSendRequest chatSendRequest) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<ChatSendRequest> request = new HttpEntity<>(chatSendRequest, headers);
-
-        ResponseEntity<String> response = restTemplate.postForEntity(aiServerUrl, request, String.class);
-
-        return response.getBody();
+        try {
+            log.info("💬 사용자 메시지 수신21: {}", chatSendRequest);
+            HttpHeaders headers = new HttpHeaders();
+            log.info("💬 사용자 메시지 수신22: {}", chatSendRequest);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            log.info("💬 사용자 메시지 수신23: {}", chatSendRequest);
+            HttpEntity<ChatSendRequest> request = new HttpEntity<>(chatSendRequest, headers);
+            log.info("💬 사용자 메시지 수신24: {}", chatSendRequest);
+            ResponseEntity<String> response = restTemplate.postForEntity(aiServerUrl, request, String.class);
+            log.info("💬 사용자 메시지 수신25: {}", chatSendRequest);
+            return response.getBody();
+        } catch (Exception e) {
+            log.error("❌ AI 서버 요청 중 예외 발생", e);
+            return "AI 서버 요청 중 오류가 발생했습니다.";
+        }
     }
 
 }
