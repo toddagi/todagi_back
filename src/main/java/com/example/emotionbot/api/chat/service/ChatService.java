d6280594932057f3ac9c253619d2628a0ba5681c
hyebin <shin378378@naver.com>
2025-04-27 02:16:34 +0900

    Refactor : ai와 소통부분 깔끔하게 만들기

--- src/main/java/com/example/emotionbot/api/chat/service/ChatService.java
+++ src/main/java/com/example/emotionbot/api/chat/service/ChatService.java
@@ -1,16 +1,32 @@
 package com.example.emotionbot.api.chat.service;
 
 import com.example.emotionbot.api.chat.entity.Chat;
+import com.example.emotionbot.api.chat.entity.ChatType;
+import com.example.emotionbot.api.chat.entity.Sender;
 import com.example.emotionbot.api.chat.repository.ChatRepository;
+import com.example.emotionbot.api.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
+
     private final ChatRepository chatRepository;
 
     public Chat saveChat(Chat chat) {
         return chatRepository.save(chat);
+    }
+
+    public Chat createChat(Member member, String message, Sender sender, ChatType type) {
+        return Chat.builder()
+                .member(member)
+                .message(message)
+                .sender(sender)
+                .type(type)
+                .sendTime(LocalDateTime.now())
+                .build();
     }
 }
