0a0dc2f8e57817888864c9a504fda31558ed289c
dnwldla <leeemjw@naver.com>
2025-04-28 13:38:17 +0900

    refactor: diary로 이름 변경

--- src/main/java/com/example/emotionbot/api/dailySummary/rest/DiaryController.java
+++ src/main/java/com/example/emotionbot/api/dailySummary/rest/DiaryController.java
@@ -3,7 +3,7 @@ package com.example.emotionbot.api.dailySummary.rest;
 import java.util.List;
 import com.example.emotionbot.api.dailySummary.dto.req.DiaryRequest;
 import com.example.emotionbot.api.dailySummary.dto.res.DiaryResponse;
-import com.example.emotionbot.api.dailySummary.service.DailySummaryService;
+import com.example.emotionbot.api.dailySummary.service.DiaryService;
 import com.example.emotionbot.common.resolver.MemberId;
 import com.example.emotionbot.common.response.APISuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,25 +16,22 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/diary")
 @Slf4j
-public class DailySummaryController {
-    private final DailySummaryService dailySummaryService;
+public class DiaryController {
+    private final DiaryService diaryService;
 
     @Operation(summary = "일기 작성", description = "일기를 작성합니다")
     @PostMapping("/create")
     public ResponseEntity<APISuccessResponse<Long>> createDiary(@MemberId Long memberId, @RequestBody DiaryRequest diaryRequest){
-        return ResponseEntity.ok(APISuccessResponse.ofCreateSuccess(dailySummaryService.saveDiary(memberId, diaryRequest)));
+        return ResponseEntity.ok(APISuccessResponse.ofCreateSuccess(diaryService.saveDiary(memberId, diaryRequest)));
     }
 
-    @Operation(summary = "일기 조회", description = "월,일별로 일기를 조회합니다")
+    @Operation(summary = "요일 별 일기 조회", description = "월별로 일기를 조회합니다")
     @GetMapping
     public ResponseEntity<APISuccessResponse<List<DiaryResponse>>> getDailySummariesByMonth(@MemberId Long memberId,@RequestParam int year, @RequestParam int month) {
-        List<DiaryResponse> dailySummaries = dailySummaryService.getDailySummariesByMonth(year,month,memberId);
+        List<DiaryResponse> dailySummaries = diaryService.getDailySummariesByMonth(year,month,memberId);
         if (dailySummaries.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(APISuccessResponse.ofSuccess(dailySummaries));
     }
-
-
-
 }
