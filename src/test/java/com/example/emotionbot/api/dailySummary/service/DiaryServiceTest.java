74921de1eae964a3cc5c1b0c2cc563e05d419191
hyebin <shin378378@naver.com>
2025-05-02 15:43:05 +0900

    Refactor : 배포를 위한 test코드 주석처리

--- src/test/java/com/example/emotionbot/api/dailySummary/service/DiaryServiceTest.java
+++ src/test/java/com/example/emotionbot/api/dailySummary/service/DiaryServiceTest.java
@@ -17,33 +17,33 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class DiaryServiceTest {
 
-    @Autowired
-    private DiaryService diaryService;
-
-    @Autowired
-    private CacheManager cacheManager;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private DailySummaryRepository dailySummaryRepository;
-
-   @Test
-    void 일기조회__캐시_작동을_확인한다(){
-       int year = 2025;
-       int month = 4;
-       Long memberId = 1L;
-
-       List<DiaryResponse> firstCallResult = diaryService.getDailySummariesByMonth(year, month, memberId);
-
-       assertThat(firstCallResult).isNotNull();
-
-       Cache diarySummaryCache = cacheManager.getCache("diarySummary");
-       assertNotNull(diarySummaryCache);
-
-       String cacheKey = String.valueOf(memberId) + ":" + year + "-" + month;
-       assertNotNull(diarySummaryCache.get(cacheKey));
-   }
+//    @Autowired
+//    private DiaryService diaryService;
+//
+//    @Autowired
+//    private CacheManager cacheManager;
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @Autowired
+//    private DailySummaryRepository dailySummaryRepository;
+//
+//   @Test
+//    void 일기조회__캐시_작동을_확인한다(){
+//       int year = 2025;
+//       int month = 4;
+//       Long memberId = 1L;
+//
+//       List<DiaryResponse> firstCallResult = diaryService.getDailySummariesByMonth(year, month, memberId);
+//
+//       assertThat(firstCallResult).isNotNull();
+//
+//       Cache diarySummaryCache = cacheManager.getCache("diarySummary");
+//       assertNotNull(diarySummaryCache);
+//
+//       String cacheKey = String.valueOf(memberId) + ":" + year + "-" + month;
+//       assertNotNull(diarySummaryCache.get(cacheKey));
+//   }
 
 }
