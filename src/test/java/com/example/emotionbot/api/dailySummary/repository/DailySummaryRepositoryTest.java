16633dd861b9ece97673455c998eb7e9c61001c5
hyebin <shin378378@naver.com>
2025-05-04 00:16:24 +0900

    Refactor: 테스트코드 주석처리

--- src/test/java/com/example/emotionbot/api/dailySummary/repository/DailySummaryRepositoryTest.java
+++ src/test/java/com/example/emotionbot/api/dailySummary/repository/DailySummaryRepositoryTest.java
@@ -12,43 +12,43 @@ import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@SpringBootTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DailySummaryRepositoryTest {
 
-    @Autowired
-    private SummaryRepositoryImpl summaryRepository;
-
-    private Long memberId;
-    private int year;
-    private int month;
-    private int day;
-    private LocalDate date;
-
-    @BeforeEach
-    void setUp() {
-        memberId = 1L;
-        year = 2025;
-        month = 12;
-        day = 10;
-        date = LocalDate.of(year, month, day);
-    }
-
-    @Test
-    void getMonthFeelingAverage_정상조회() {
-        MonthResponse.AverageFeeling averageFeeling = summaryRepository.getAverageFeeling(memberId, year, month);
-        System.out.println(averageFeeling);
-    }
-
-    @Test
-    void 날짜별_감정을_반환한다() {
-        assertThat(summaryRepository.getDailyFeeling(memberId, year, month)).isNotNull();
-    }
-
-
-    @Test
-    void 주차별_감정을_반환한다() {
-        assertThat(summaryRepository.getWeeklyFeeling(memberId, date)).size().isEqualTo(7);
-    }
+//    @Autowired
+//    private SummaryRepositoryImpl summaryRepository;
+//
+//    private Long memberId;
+//    private int year;
+//    private int month;
+//    private int day;
+//    private LocalDate date;
+//
+//    @BeforeEach
+//    void setUp() {
+//        memberId = 1L;
+//        year = 2025;
+//        month = 12;
+//        day = 10;
+//        date = LocalDate.of(year, month, day);
+//    }
+//
+//    @Test
+//    void getMonthFeelingAverage_정상조회() {
+//        MonthResponse.AverageFeeling averageFeeling = summaryRepository.getAverageFeeling(memberId, year, month);
+//        System.out.println(averageFeeling);
+//    }
+//
+//    @Test
+//    void 날짜별_감정을_반환한다() {
+//        assertThat(summaryRepository.getDailyFeeling(memberId, year, month)).isNotNull();
+//    }
+//
+//
+//    @Test
+//    void 주차별_감정을_반환한다() {
+//        assertThat(summaryRepository.getWeeklyFeeling(memberId, date)).size().isEqualTo(7);
+//    }
 
 }
