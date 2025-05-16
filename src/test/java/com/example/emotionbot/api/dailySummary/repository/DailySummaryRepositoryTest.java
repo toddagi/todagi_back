18a44e2389663332beb075d4eb0611e0ca7c8bca
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-05-16 16:20:33 +0900

    Update DailySummaryRepositoryTest.java

--- src/test/java/com/example/emotionbot/api/dailySummary/repository/DailySummaryRepositoryTest.java
+++ src/test/java/com/example/emotionbot/api/dailySummary/repository/DailySummaryRepositoryTest.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
